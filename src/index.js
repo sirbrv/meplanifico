4c5d3a2d5838b60abbe90f6af297e4418f9072e6
Victor Brown <sirbrv@gmail.com>
2023-04-08 18:50:52 -0300

    Actualización

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
+//import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
