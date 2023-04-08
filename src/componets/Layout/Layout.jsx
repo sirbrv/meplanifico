578ccb7313eeff467e456e44fdcbbc7dd8382dd1
Victor Brown <sirbrv@gmail.com>
2023-04-08 19:07:13 -0300

    Actualización

---
+++ src/componets/Layout/Layout.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Sidebar from "../../componets/sidebar/Sidebar";
+//import Footer from "../../pages/footer/Footer";
+import Container from "react-bootstrap/Container";
+import "../../../src/App.css";
+
+const Layout = ({ children }) => {
+  const [closesidebar, setCloseSidebar] = useState(false);
+  return (
+    <>
+      <Sidebar closesidebar={closesidebar} />
+      <main onClick={() => setCloseSidebar(!closesidebar)}>
+        <Container fluid>{children}</Container>
+      </main>
+      {/* <Footer /> */}
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/componets/sidebar/SubMenu.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+const SubMenu = ({ item, setSidebar }) => {
+  const [subnav, setSubnav] = useState(false);
+
+  const showSubnav = () => {
+    setSubnav(!subnav);
+  };
+
+  const showLink = () => {
+    setSidebar(false);
+  };
+
+  return (
+    <>
+      {!item.path ? (
+        <div onClick={item.subNav && showSubnav} className="sidebarSubMenu">
+          <div className="sidebarTitle">
+            {item.icon}
+            <div className="sidebarLabel">{item.title}</div>
+          </div>
+          <div>
+            {item.subNav && subnav
+              ? item.iconOpened
+              : item.subNav
+              ? item.iconClosed
+              : null}
+          </div>
+        </div>
+      ) : (
+        <Link to={item.path} onClick={showLink} className="sidebarLink">
+          <div className="sidebarTitle">
+            {item.icon}
+            <div className="sidebarLabel">{item.title}</div>
+          </div>
+          <div>
+            {item.subNav && subnav
+              ? item.iconOpened
+              : item.subNav
+              ? item.iconClosed
+              : null}
+          </div>
+        </Link>
+      )}
+
+      {subnav &&
+        item.subNav.map((item, index) => {
+          return (
+            <Link
+              to={item.path}
+              key={index}
+              onClick={showLink}
+              className="dropdownLink"
+            >
+              {item.icon}
+              <div className="sidebarLabel">{item.title}</div>
+            </Link>
+          );
+        })}
+    </>
+  );
+};
+
+export default SubMenu;
