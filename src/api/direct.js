159cbbcb76f52dc24fdf1d8f98e38dae32041551
Victor Brown <sirbrv@gmail.com>
2023-04-09 20:23:45 -0300

    Carga inicial del proyecto

--- src/api/direct.js
+++ src/api/direct.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const port = process.env.PORT || "4000";
-let BASE_URL = "https://meplanificobackend.herokuapp.com/";
+let BASE_URL = "https://meplanificobackend.herokuapp.com";
 // eslint-disable-next-line
 if (port == 4000) {
   BASE_URL = "http://localhost:4000";
