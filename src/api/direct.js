1445b5be319dfd280e714812deb59f5f5aaaadf6
Victor Brown <sirbrv@gmail.com>
2023-04-14 11:15:09 -0300

    Actualiza acceso a puerto

--- src/api/direct.js
+++ src/api/direct.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
-const port = process.env.PORT || 4000;
+const port = process.env.REACT_APP_PORT || 4000;
 let BASE_URL = "https://meplanificobackend.herokuapp.com";
 // eslint-disable-next-line
+console.log("En..:", port);
 
-if (port === 4000) {
+if (parseInt(port) === 4000) {
   BASE_URL = "http://localhost:4000";
 }
 
-console.log("En..:", process.env.PORT, "....:", BASE_URL);
+console.log("En..:", process.env.REACT_APP_PORT, "....:", BASE_URL);
 export default axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
