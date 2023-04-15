598e3d30f1a5d3fb64ec255da51517dca3e1efe7
Victor Brown <sirbrv@gmail.com>
2023-04-15 17:16:04 -0300

    Actualiza acceso al puerto

--- src/pages/report/EdaCuenta.jsx
+++ src/pages/report/EdaCuenta.jsx
@@ -304,7 +304,7 @@ function EdoCuentas() {
                               as="select"
                               name="year"
                               onChange={inputsHandler}
-                              value={yearActual}
+                            //  value={yearActual}
                               //    disabled={disabledFecha}
                             >
                               {selectYear.map((option) =>
@@ -442,27 +442,27 @@ function EdoCuentas() {
 }
 
 const selectYear = [
-  { id: 0, year: "2020" },
-  { id: 1, year: "2021" },
-  { id: 2, year: "2022" },
-  { id: 3, year: "2023" },
-  { id: 4, year: "2024" },
-  { id: 5, year: "2025" },
-  { id: 6, year: "2026" },
-  { id: 7, year: "2027" },
-  { id: 8, year: "2028" },
-  { id: 9, year: "2029" },
-  { id: 10, year: "2030" },
-  { id: 11, year: "2031" },
-  { id: 12, year: "2032" },
-  { id: 13, year: "2033" },
-  { id: 14, year: "2034" },
-  { id: 15, year: "2035" },
-  { id: 16, year: "2036" },
-  { id: 17, year: "2037" },
-  { id: 18, year: "2008" },
-  { id: 19, year: "2039" },
-  { id: 20, year: "2040" },
+  { id: 0, year: 2020 },
+  { id: 1, year: 2021 },
+  { id: 2, year: 2022 },
+  { id: 3, year: 2023 },
+  { id: 4, year: 2024 },
+  { id: 5, year: 2025 },
+  { id: 6, year: 2026 },
+  { id: 7, year: 2027 },
+  { id: 8, year: 2028 },
+  { id: 9, year: 2029 },
+  { id: 10, year: 2030 },
+  { id: 11, year: 2031 },
+  { id: 12, year: 2032 },
+  { id: 13, year: 2033 },
+  { id: 14, year: 2034 },
+  { id: 15, year: 2035 },
+  { id: 16, year: 2036 },
+  { id: 17, year: 2037 },
+  { id: 18, year: 2038 },
+  { id: 19, year: 2039 },
+  { id: 20, year: 2040 },
 ];
 
 const selectMes = [
